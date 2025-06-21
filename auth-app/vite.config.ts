58ddcccad227ebfbb0cde368ba0be56aebc81927
yujhenchen <yujhen.chen93@gmail.com>
2025-06-21 17:38:35 +0800

    feat: vike js project files

---
+++ auth-app/vite.config.ts
@@ -0,0 +1,19 @@
+import vercel from "vite-plugin-vercel";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import vike from "vike/plugin";
+
+export default defineConfig({
+  plugins: [vike(), react(), tailwindcss(), vercel()],
+
+  build: {
+    target: "es2022",
+  },
+
+  resolve: {
+    alias: {
+      "@": new URL("./", import.meta.url).pathname,
+    },
+  },
+});
